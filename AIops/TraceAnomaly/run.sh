2ff011ce9a41bf76110590ba4016521d031ea32e
sleep_ben <143080974+aurora-er@users.noreply.github.com>
2024-07-04 10:42:41 +0800

    Add files via upload

---
+++ AIops/TraceAnomaly/run.sh
@@ -0,0 +1,2 @@
+python -m traceanomaly.main --trainpath ./train_ticket/train --normalpath ./train_ticket/test_normal_small --abnormalpath ./train_ticket/test_abnormal_sm
+all --outputpath result -c flow_type=rnvp
